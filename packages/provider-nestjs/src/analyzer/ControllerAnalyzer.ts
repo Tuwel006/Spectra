fc10b1faa1e0c8008c91ce8f5eefbe0dad5653d9
Tuwel Shaikh <tuwel@karmicksolutions.com>
2026-07-21 17:57:21 +0530

    feat(provider-nestjs): add controller analyzer

--- packages/provider-nestjs/src/analyzer/ControllerAnalyzer.ts
+++ packages/provider-nestjs/src/analyzer/ControllerAnalyzer.ts
@@ -1,23 +1,41 @@
 import ts from "typescript";
-import {
-    AstProject,
-    NodeWalker,
-    ClassQuery,
-} from "@spectra/provider-ast";
+import { ClassQuery } from "@spectra/provider-ast";
+
+import { ControllerMetadata } from "../metadata";
+import { DecoratorReader } from "../utils";
 
 export class ControllerAnalyzer {
 
-    private readonly classQuery: ClassQuery;
-
     public constructor(
-        project: AstProject,
-    ) {
+        private readonly classQuery: ClassQuery,
+        private readonly decoratorReader: DecoratorReader,
+    ) { }
 
-        const walker = new NodeWalker();
+    public analyze(
+        sourceFile: ts.SourceFile,
+    ): readonly ControllerMetadata[] {
 
-        this.classQuery = new ClassQuery(
-            walker,
-        );
+        const controllers: ControllerMetadata[] = [];
+
+        const classes = this.classQuery.execute(sourceFile);
+
+        for (const classNode of classes) {
+
+            if (!this.decoratorReader.has(classNode, "Controller")) {
+                continue;
+            }
+
+            controllers.push({
+                name: classNode.name?.text ?? "Anonymous",
+                path: "",
+                classNode,
+                tags: [],
+                routes: [],
+            });
+
+        }
+
+        return controllers;
 
     }
 
